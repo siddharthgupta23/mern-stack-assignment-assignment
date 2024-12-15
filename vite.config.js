adce0bcc844bc3b6ab32e0a645d8a2897a4ecc3b
Siddharth Gupta <siddharthgupta1603@gmail.com>
2024-12-16 00:33:46 +0530

    initial branch

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       external: ['react-router-dom'],
+      input: './index.html',
     },
-  }
+  },
+  base: './',
 })
